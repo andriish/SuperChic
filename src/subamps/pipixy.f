8278b06743d945326f725e9f053b1430b1461331
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2023-11-16 08:47:49 +0100

    OK

--- src/subamps/pipixy.f
+++ src/subamps/pipixy.f
@@ -2,12 +2,15 @@ ccc   gg --> pipi subprocess amplitude (x,y dependent)
       subroutine pipixy(x,y,cost,out)
       implicit none
       double precision x,y,out,cost,a,b
+      double precision b2,c2 
+       
+      a= 1d0-x-y+2*x*y
+      b= 1d0-x-y
+      b2 = b**2
+      c2 = cost**2
 
-      a=(1d0-x)*(1d0-y)+x*y
-      b=(1d0-x)*(1d0-y)-x*y
-
-      out=cost**2-2d0*4d0/3d0/3d0*a
-      out=out*(a-b**2)/(a**2-b**2*cost**2)
+      out=c2-8d0/9d0*a
+      out=out*(a-b2)/(a**2-b2*c2)
 
       return
       end
