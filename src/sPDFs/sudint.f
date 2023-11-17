174af6318fb0fc36d172587588c45cd4246c43ce
andriish <andriish@users.noreply.github.com>
2023-11-17 12:18:36 +0100

    Update sudint.f

--- src/sPDFs/sudint.f
+++ src/sPDFs/sudint.f
@@ -4,9 +4,7 @@ ccc   interpolator for Sudakov factor
       integer i,j
       double precision iqinc,iminc
 
-      include 'sudpars.f'
-      iqinc=1.0d0/qinc 
-      iminc=1.0d0/minc 
+      include 'sudpars.f' 
       mx=dlog(mxx)
       iqinc=1.0d0/qinc 
       iminc=1.0d0/minc 
