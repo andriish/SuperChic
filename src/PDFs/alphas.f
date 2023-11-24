c4d635aff9434487c4147cf57519448f76baca11
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2023-11-24 13:25:55 +0100

    OK

--- src/PDFs/alphas.f
+++ src/PDFs/alphas.f
@@ -2,10 +2,10 @@ ccc   strong coupling constant, for PDFINPUT = LHAPDF
       function alphas(qsq)
       implicit double precision (a-z)
 
-       if (dsq.le.1.d0) then 
-!       write(*,*)'Error in alphas',dsq
-       alphas=alphasPDF(1.0d0)
-       else
+      if (qsq.le.1.d0) then 
+      write(*,*)'Error in alphas.f qsq=',qsq
+      alphas=alphasPDF(1.0d0)
+      else
       alphas=alphasPDF(dsqrt(qsq))
       end if
 
