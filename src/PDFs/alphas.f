a2b38128974dcefc7c87f6494b7841e0ab1ecba0
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2023-11-24 14:30:28 +0100

    OK

--- src/PDFs/alphas.f
+++ src/PDFs/alphas.f
@@ -1,10 +1,14 @@
 ccc   strong coupling constant, for PDFINPUT = LHAPDF
       function alphas(qsq)
       implicit double precision (a-z)
+      include 'pdfinf.f'
 
-      if (qsq.le.1.d0) then 
-      write(*,*)'Error in alphas.f qsq=',qsq
-      alphas=alphasPDF(1.0d0)
+      if (qsq.lt.pdfq2min) then 
+      if ( pdfwarn.ge.0) then
+      write(*,*)'Error in alphas.f qsq=',qsq,' < ',pdfq2min,pdfwarn
+      pdfwarn=pdfwarn-1
+      endif
+      alphas=alphasPDF(pdfq2min)
       else
       alphas=alphasPDF(dsqrt(qsq))
       end if
--- src/PDFs/inpdf.f
+++ src/PDFs/inpdf.f
@@ -1,6 +1,7 @@
       subroutine inpdf
 
       include 'pdfinf.f'
+      
 
 cccccccccccccccccccccccccccccccccccccccccccccccccccc
 ccccccccc   Init LHAPDF
@@ -11,6 +12,7 @@ cccccccccccccccccccccccccccccccccccccccccccccccccccc
       call initpdfset
      &     ('PDFsets/'//PDFname)
       call initpdf(PDFmember)
-
+      call getq2min(PDFmember,pdfq2min)
+      pdfwarn=10
       return
       end
